be5805ea7cd531a54d0687a01ae0275e224271d6
Eugenya <water1983@yandex.ru>
2019-07-02 23:23:54 +0300

    hometask_2 builder

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.devintensive.models
 
+import ru.skillbranch.devintensive.extensions.format
 import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
 
@@ -25,7 +26,7 @@ data class User(
             "It's Alive!!! \n" +
                     "${if (lastName == "Doe")
                         "His name is $firstName $lastName"
-                       else "And his name is $lastName $firstName"} "
+                    else "And his name is $lastName $firstName"} "
         )
     }
 
@@ -37,7 +38,7 @@ data class User(
         avatar: $avatar
         rating: $rating
         respect: $respect
-        lastVisit: $lastVisit
+        lastVisit: ${lastVisit?.format()}
         isOnline: $isOnline
               """.trimIndent()
     )
@@ -54,4 +55,27 @@ data class User(
 
         }
     }
+
+    class Builder {
+        var id: String = "0"
+        var firstName: String? = ""
+        var lastName: String? = ""
+        var avatar: String? = ""
+        var rating: Int = 0
+        var respect: Int = 0
+        var lastVisit: Date? = Date()
+        var isOnline: Boolean = false
+
+        fun id(id: String) = apply { this.id = id }
+        fun firstName(firstName: String) = apply { this.firstName = firstName }
+        fun lastName(lastName: String) = apply { this.lastName = lastName }
+        fun avatar(avatar: String) = apply { this.avatar = avatar }
+        fun rating(rating: Int) = apply { this.rating = rating }
+        fun respect(respect: Int) = apply { this.respect = respect }
+        fun lastVisit(lastVisit: Date) = apply { this.lastVisit = lastVisit }
+        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
+        fun build() = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
+
+    }
+
 }
