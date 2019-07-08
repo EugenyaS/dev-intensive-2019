f1c9b9679fffe60e229d093ef2699a1afff19031
Eugenya <water1983@yandex.ru>
2019-07-08 19:39:02 +0300

    hometask_2 stripHtml

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -5,3 +5,10 @@ fun String.truncate(lengthStr: Int = 16): String  = when (trimEnd().length - 1) 
         in 0..lengthStr -> trimEnd()
         else -> trimEnd().substring(0 until lengthStr).trimEnd().plus("...")
     }
+
+fun String.stripHtml():String {
+    var result=this
+    result = result.replace("[ ]+".toRegex(), " ").replace("&(?:[a-z\\d]+|#\\d+|#x[a-f\\d]+);".toRegex(), "")
+        .replace("<(.|\\n)*?>".toRegex(), "")
+    return result
+}
