c34a6025e9fa9a8c70a463b6aefa56dfcfc35d86
Eugenya <water1983@yandex.ru>
2019-07-14 23:34:27 +0300

    hometask_3 validator of answer

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -12,11 +12,14 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     }
 
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
-        return if (question==Question.IDLE  || question.answers.contains(answer)) {
+        val validatorResult = question.validateAnswer(answer)
+        return if (validatorResult != "") {
+            return validatorResult to status.color
+        } else if (question == Question.IDLE || question.answers.contains(answer.toLowerCase())) {
             question = question.nextQuestion()
             "Отлично - ты справился\n${question.question}" to status.color
         } else {
-            if (status==Status.CRITICAL){
+            if (status == Status.CRITICAL) {
                 status = Status.NORMAL
                 question = Question.NAME
                 "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
@@ -45,24 +48,53 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     enum class Question(val question: String, val answers: List<String>) {
         NAME("Как меня зовут?", listOf("бендер", "bender")) {
             override fun nextQuestion(): Question = PROFESSION
+            override fun validateAnswer(answer: String): String {
+                if (!Regex("""^[A-ZА-Я].*$""").matches(answer)) {
+                    return "Имя должно начинаться с заглавной буквы"
+                } else return ""
+            }
         },
         PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
             override fun nextQuestion(): Question = MATERIAL
+            override fun validateAnswer(answer: String): String {
+                if (!Regex("""^[a-zа-я].*$""").matches(answer)) {
+                    return "Профессия должна начинаться со строчной буквы"
+                } else return ""
+            }
         },
         MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
             override fun nextQuestion(): Question = BDAY
+            override fun validateAnswer(answer: String): String {
+                if (!Regex("""^[^0-9]*$""").matches(answer)) {
+                    return "Материал не должен содержать цифр"
+                } else return ""
+            }
         },
         BDAY("Когда меня создали?", listOf("2993")) {
             override fun nextQuestion(): Question = SERIAL
+            override fun validateAnswer(answer: String): String {
+                if (!Regex("""^[0-9]*$""").matches(answer)) {
+                    return "Год моего рождения должен содержать только цифры"
+                } else return ""
+            }
         },
         SERIAL("Мой серийный номер?", listOf("2716057")) {
             override fun nextQuestion(): Question = IDLE
+            override fun validateAnswer(answer: String): String {
+                if (!Regex("""^[0-9]{7}$""").matches(answer)) {
+                    return "Серийный номер содержит только цифры, и их 7"
+                } else return ""
+            }
         },
         IDLE("На этом все, вопросов больше нет", listOf()) {
             override fun nextQuestion(): Question = IDLE
+            override fun validateAnswer(answer: String): String {
+                return ""
+            }
         };
 
         abstract fun nextQuestion(): Question
+        abstract fun validateAnswer(answer: String): String
     }
 
 }
